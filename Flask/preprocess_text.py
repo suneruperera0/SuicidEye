d9b9a6688111b1ebf2300f8826c32b24497bd7ab
husitech <admin@husi.ca>
2024-01-05 11:29:43 -0500

    first commit

---
+++ Flask/preprocess_text.py
@@ -0,0 +1,23 @@
+import re
+from nltk.stem import WordNetLemmatizer
+
+from Flask.app import sp
+
+all_stopwords = sp.Defaults.stop_words
+
+new_words = ["http", "www", "co", "u", "com", "t", "s", "m",
+             "ve", "dy", "ll", 'n', 'r', 'b', "wa", "y", "don", "ha"]
+for words in new_words:
+    all_stopwords.add(words)
+
+wordnet = WordNetLemmatizer()
+
+
+def clean_text(text, stopwords=all_stopwords):
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = text.lower()
+    text = text.split(' ')
+    text = [wordnet.lemmatize(word) for word in text]
+    text = [word for word in text if word not in stopwords]
+    text = ' '.join(text)
+    return text
